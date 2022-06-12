4ea81509605a3ab162bfd37cc80278c61e2c716a
Fitrianii <fitrianikz28@gmail.com>
2022-06-12 20:30:27 +0800

    landing page

--- frontend/src/screens/Pages.jsx
+++ frontend/src/screens/Pages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
+import LandingPage from "../components/LandingPage/LandingPage";
 
 const Pages = () => {
   return (
@@ -9,6 +10,7 @@ const Pages = () => {
       <Routes>
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/landingpage" element={<LandingPage />} />
       </Routes>
     </div>
   );
