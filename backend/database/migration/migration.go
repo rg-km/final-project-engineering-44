83b1c88615db4999abbabe1df4ccdb75a964e9aa
anton <dafiqalimi17@gmail.com>
2022-06-15 14:06:39 +0700

    database

--- backend/database/migration/migration.go
+++ backend/database/migration/migration.go
@@ -9,31 +9,54 @@ import (
 )
 
 func main() {
-	db, err := database.Connect()
+	db, err := sql.Open("sqlite3", "./user.db")
 	if err != nil {
 		panic(err)
 	}
+	_, err = db.Exec(`
+	CREATE TABLE users(
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+		Name VARCHAR NOT NULL,
+		Email VARCHAR NOT NULL,
+		Jenjang VARCHAR NOT NULL,
+		Domisili VARCHAR NOT NULL,
+		Password VARCHAR NOT NULL,
+		Role VARCHAR CHECK(role IN ('admin', 'user')) NOT NULL DEFAULT 'user',
+		Created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	);
+
+	CREATE TABLE scholarships(
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+		User_id INTEGER,
+		Name VARCHAR(255) NOT NULL,
+		Description TEXT NOT NULL,
+		Jenjang VARCHAR(255) NOT NULL,
+		Domisili VARCHAR(255) NOT NULL,
+		Image VARCHAR DEFAULT 'image.jpg',
+		Category_id INTEGER NOT NULL,
+		Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (`User_id`) REFERENCES `users` (`id`)
+		FOREIGN KEY (`Category_id`) REFERENCES `categories` (`id`)
+	  );
+	  CREATE TABLE categories (
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+		Category_name VARCHAR(255) NOT NULL,
+		Created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	  );
+	  
+	  CREATE TABLE comments (
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+		Content TEXT NOT NULL,
+		User_id INTEGER NOT NULL,
+		Scholarship_id INTEGER NOT NULL,
+		Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (`User_id`) REFERENCES `users` (`id`),
+		FOREIGN KEY (`Scholarship_id`) REFERENCES `scholarships` (`id`)
+	  );`
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database created")
 	defer db.Close()
-
-	// Create the table
-	res, err := CreateTableUser(db)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(res)
-}
-
-func CreateTableUser(db *sql.DB) (string, error) {
-	_, err := db.Exec(`
-		CREATE TABLE IF NOT EXISTS user (
-			Id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-			Username varchar(255) NOT NULL,
-			Email varchar(255) NOT NULL UNIQUE,
-			Password varchar(255) NOT NULL,
-			Role varchar(255) NOT NULL
-	);`)
-	if err != nil {
-		return "Error : ", err
-	}
-	return "Success : ", nil
 }
