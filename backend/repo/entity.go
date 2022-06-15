2908afcc2ee2eff68023723532692667aa4651c1
anton <dafiqalimi17@gmail.com>
2022-06-15 14:36:51 +0700

    g

--- backend/repo/entity.go
+++ backend/repo/entity.go
@@ -1,9 +1,15 @@
 package repo
 
+import "time"
+
 type User struct {
-	Id       int    `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
+	Id        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Jenjang   string    `json:"jenjang"`
+	Domisili  string    `json:"Domisili"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
