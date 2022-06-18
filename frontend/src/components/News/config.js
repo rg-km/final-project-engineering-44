1eafa1b174d789335563d63b6b05ed3126c617bd
dap23 <daffa230601@gmail.com>
2022-06-18 15:59:08 +0700

    content :3

--- frontend/src/components/News/config.js
+++ frontend/src/components/News/config.js
@@ -1,4 +1,3 @@
-const API_KEY = "88fb7be1810042d8afedb2a63a25d25f";
-const URL = `https://newsapi.org/v2/everything?q=scholarship&apiKey=${API_KEY}`;
+const URL = `https://api.imgflip.com/get_memes`;
 
 export default URL;
