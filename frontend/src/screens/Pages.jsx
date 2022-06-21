7ca27a94b7514410e078db5064f98bbed3ef59dc
Fitrianii <fitrianikz28@gmail.com>
2022-06-21 08:58:23 +0800

    tmapilan index admin

--- frontend/src/screens/Pages.jsx
+++ frontend/src/screens/Pages.jsx
@@ -8,6 +8,7 @@ import Scholarship from "../components/Scholarship/Scholarship";
 import SidebarNews from "../components/SidebarNews/SidebarNews";
 import Beasiswa from "../pages/Beasiswa/Beasiswa";
 import ProfilPage from "../pages/ProfilPage/ProfilPage";
+import IndexAdmin from "../pages/Index/IndexAdmin";
 
 const Pages = () => {
   return (
@@ -21,6 +22,7 @@ const Pages = () => {
         <Route path="/news" element={<SidebarNews />} />
         <Route path="/beasiswa" element={<Beasiswa />} />
         <Route path="/profil" element={<ProfilPage />} />
+        <Route path="/index-admin" element={<IndexAdmin />} />
       </Routes>
     </div>
   );
