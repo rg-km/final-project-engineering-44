ef62c15ba84cbc7748329974eea47cc7ddf48daa
anton <dafiqalimi17@gmail.com>
2022-06-22 16:45:01 +0700

    revisi

--- backend/api/request.go
+++ backend/api/request.go
@@ -4,11 +4,3 @@ type reqLogin struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
-
-type reqRegister struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Jenjang  string `json:"jenjang"`
-	Kota     string `json:"kota"`
-}
